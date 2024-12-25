26b8b25d1efeaf9aea09dcf5ab55d4cf8723b4b3
somapmk96 <somapmk96@gmail.com>
2024-12-25 17:19:46 +0530

    POJO commit

--- src/test/java/runner/Junit.java
+++ src/test/java/runner/Junit.java
@@ -1,36 +1,36 @@
-package runner;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
-
-
-public class Junit {
-    @Test
-    public void testFindMax() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\soman\\Workbench\\ChromeDriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://seleniumpractise.blogspot.com/2021/08/webtable-in-html.html");
-
-        List<WebElement> elements = driver.findElements(By.xpath("//table[contains(@id,\"cus\")]//tr"));
-
-        List<WebElement> countires = driver.findElements(By.xpath("//table[contains(@id,'cus')]//tr/td[2]"));
-        int i;
-        for (i = 0; i < countires.size(); i++) {
-            WebElement country = countires.get(i);
-            String countryName = country.getText();
-            if (countryName.equalsIgnoreCase("India")) {
-                driver.findElement(By.xpath("//table[contains(@id,'cus')]//tr[" + i + "]//td[5]")).click();
-                break;
-            }
-        }
-        System.out.println("I:" + i);
-        driver.quit();
-
-    }
-}
+//package runner;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import java.util.List;
+//
+//
+//public class Junit {
+//    @Test
+//    public void testFindMax() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\soman\\Workbench\\ChromeDriver\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.navigate().to("https://seleniumpractise.blogspot.com/2021/08/webtable-in-html.html");
+//
+//        List<WebElement> elements = driver.findElements(By.xpath("//table[contains(@id,\"cus\")]//tr"));
+//
+//        List<WebElement> countires = driver.findElements(By.xpath("//table[contains(@id,'cus')]//tr/td[2]"));
+//        int i;
+//        for (i = 0; i < countires.size(); i++) {
+//            WebElement country = countires.get(i);
+//            String countryName = country.getText();
+//            if (countryName.equalsIgnoreCase("India")) {
+//                driver.findElement(By.xpath("//table[contains(@id,'cus')]//tr[" + i + "]//td[5]")).click();
+//                break;
+//            }
+//        }
+//        System.out.println("I:" + i);
+//        driver.quit();
+//
+//    }
+//}
--- src/test/java/runner/demo/Solution.java
+++ src/test/java/runner/demo/Solution.java
@@ -1,18 +1,31 @@
-package runner.demo;
-
-import java.util.HashMap;
-
-public class Solution {
-    public static void main(String[] args) {
-        String val = "aabbcdde";
-        HashMap<String, Integer> count = new HashMap<>();
-        for (String str : val.split("")) {
-           if(count.containsKey(str)) {
-               count.put(str, count.get(str) + 1);
-           }else {
-               count.put(str, 1);
-           }
-        }
-        System.out.println(count);
-    }
-}
+//package runner.demo;
+//
+//import io.restassured.RestAssured;
+//import io.restassured.path.json.JsonPath;
+//import io.restassured.response.Response;
+//import io.restassured.specification.RequestSpecification;
+//import org.junit.Assert;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Solution {
+//    public static void main(String[] args) {
+//        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Book?ISBN=9781449325862";
+//        RequestSpecification httpRequest = RestAssured.given().pa
+//
+//        // First get the JsonPath object instance from the Response interface
+//        JsonPath jsonPathEvaluator = response.jsonPath();
+//
+//        // Then simply query the JsonPath object to get a String value of the node
+//        // specified by JsonPath: City (Note: You should not put $. in the Java code)
+//        String city = jsonPathEvaluator.get("City");
+//
+//        // Let us print the city variable to see what we got
+//        System.out.println("City received from Response " + city);
+//
+//        // Validate the response
+//        Assert.assertEquals(city, "Hyderabad", "Correct city name received in the Response");
+//
+//    }
+//}
